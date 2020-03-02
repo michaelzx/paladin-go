eaf1a6f2fce68439933dbf14b92c002b5a68c552
michaelzx <mail@zhanxiao.org>
2020-03-02 21:44:29 +0800

    增加通用错误

--- errs/bad_request_errors.go
+++ errs/bad_request_errors.go
@@ -6,4 +6,5 @@ var (
 	DataAlreadyExist = NewBadRequestErrorWithCode(10002, "已存在")
 	ParamsNotExist   = NewBadRequestErrorWithCode(10003, "缺少参数：")
 	ParamsErr        = NewBadRequestErrorWithCode(10004, "参数错误：")
+	ParamsRequired   = NewBadRequestErrorWithCode(10005, "缺少必填项：")
 )
